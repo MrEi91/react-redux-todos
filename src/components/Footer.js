81c11fce0174a94c5e432cb09c6f2571ded3d035
Eri Irawan <eryirawan91@gmail.com>
2017-04-01 12:41:57 +0700

    add commponent footer

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+  render(){
+    return (
+      <footer className="page-footer">
+        <div className="footer-copyright">
+          <div className="container">
+          © 2017 Copyright <a href="https://github.com/MrEi91">Eri Irawan</a>
+          </div>
+        </div>
+      </footer>
+    )
+  }
+}
+
+export default Footer
