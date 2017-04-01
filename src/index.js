2e3c7e3b742bd1415daea0e331e7231d58fd72e0
Eri Irawan <eryirawan91@gmail.com>
2017-04-01 12:40:35 +0700

    createStore, imort logger and thunk

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+
+import App from './App'
+import './index.css'
+import rootReducer from './reducers'
+
+
+const store = createStore(rootReducer, compose(
+  applyMiddleware(thunk, logger),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+))
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
-);
+)
