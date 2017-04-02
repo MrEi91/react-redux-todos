5d2c262132959edda5a09e00471ad0ade3f461e4
Eri Irawan <eryirawan91@gmail.com>
2017-04-02 22:21:08 +0700

    add function search todo

--- src/actions/todoActionCreator.js
+++ src/actions/todoActionCreator.js
@@ -1,5 +1,12 @@
 import * as ActionTypes from './constants'
 
+export const searchTodo = todo => {
+  return {
+    type: 'SEARCH_TODO',
+    payload: todo
+  }
+}
+
 export const getTodos = todos => {
   return {
     type: ActionTypes.GET_TODOS,
@@ -11,6 +18,30 @@ export const actionAddTodo = todo => {
   return {
     type: ActionTypes.ADD_TODO,
     payload:todo
+  }
+}
+
+export const actionUpdateTodo = (id, todo) => {
+  return {
+    type: ActionTypes.UPDATE_TODO,
+    payload:{
+      id:id,
+      title:todo
+    }
+  }
+}
+
+// export const actionUpdateCompleteTodo = is_completed => {
+//   return {
+//     type: ActionTypes.UPDATE_COMPLETE_TODO,
+//     payload: is_completed
+//   }
+// }
+
+export const actionDeleteTodo = id => {
+  return {
+    type: ActionTypes.DELETE_TODO,
+    payload:id
   }
 }
 
@@ -41,5 +72,59 @@ export const addTodo = (todo) => {
     })
     .then(response => response.json())
     .then(todo => dispatch(actionAddTodo(todo)))
+  }
+}
+
+export const updateTodo = (id, title) => {
+  return (dispatch) => {
+    fetch('http://localhost:4000/todos/' + id,{
+      method:'PUT',
+      body:JSON.stringify({
+        title: title,
+        is_completed: false
+      }),
+      headers:{
+        'Content-Type' : 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then((id, todo) => dispatch(actionUpdateTodo(id, todo)))
+    .catch(err =>  console.log(err))
+  }
+}
+
+// export const updateCompleteTodo = (id, title, is_completed) => {
+//
+//   let isTrueSet = (is_completed === "true")
+//   let getStatus = (isTrueSet) ? "false" : "true"
+//
+//   return (dispatch) => {
+//     fetch('http://localhost:4000/todos/' + id,{
+//       method:'PUT',
+//       body:JSON.stringify({
+//         id: id,
+//         title: title,
+//         is_completed: getStatus
+//       }),
+//       headers:{
+//         'Content-Type' : 'application/json'
+//       }
+//     })
+//     .then(response => response.json())
+//     .then(todo => dispatch(actionUpdateCompleteTodo(id, todo)))
+//   }
+// }
+
+export const deleteTodo = (id) => {
+  return (dispatch) => {
+    fetch('http://localhost:4000/todos/' + id,{
+      method:'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(todo => dispatch(actionDeleteTodo(id)))
+    .catch(err => console.log(err))
   }
 }
