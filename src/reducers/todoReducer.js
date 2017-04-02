866b664b17e2be5086669b8bf7dc7f2227a1f05f
Eri Irawan <eryirawan91@gmail.com>
2017-04-02 22:23:01 +0700

    add switch case action type search todo

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -1,19 +1,36 @@
 import * as ActionTypes from '../actions/constants'
 
-const initialState = {todos:[], todosFilter:[]}
+const initialState = {todos:[], filterTodos:[]}
 
 const todoReducer = (state = initialState, action) => {
   switch (action.type) {
     case ActionTypes.GET_TODOS:
       return Object.assign({}, state, {
         todos:action.payload,
-        todosFilter: action.payload
+        filterTodos: action.payload
       })
     case ActionTypes.ADD_TODO:
       return Object.assign({}, state, {
         todos:[...state.todos, action.payload],
-        todosFilter:action.payload
+        filterTodos:action.payload
       })
+    case ActionTypes.DELETE_TODO:
+      let newState = state.todos.filter((todo) => {
+        return todo.id !== action.payload
+      })
+      return Object.assign({}, state, {
+        todos: newState,
+        filterTodos: newState
+      })
+    case ActionTypes.UPDATE_TODO:
+
+      return
+    case ActionTypes.SEARCH_TODO:
+      const filterTodos = state.todos.filter((todo, index) => {
+        const patternFilter = new RegExp(action.payload, 'gi')
+        return patternFilter.test(todo.title)
+      })
+      return Object.assign({}, state, {filterTodos})
     default:
       return state
   }
