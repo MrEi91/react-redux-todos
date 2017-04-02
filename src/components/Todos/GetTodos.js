487677d71f44b0268fe0abac702b56092495b1f0
Eri Irawan <eryirawan91@gmail.com>
2017-04-02 22:22:13 +0700

    add dispatch update, search and text field search and manipulation edit, but edit on progress

--- src/components/Todos/GetTodos.js
+++ src/components/Todos/GetTodos.js
@@ -1,42 +1,72 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-import { fetchTodos } from '../../actions'
+import { fetchTodos, deleteTodo, updateTodo, searchTodo } from '../../actions'
 
 class GetTodos extends Component {
-  // constructor(props){
-  //   super(props)
-  //   this.setState = ({
-  //
-  //   })
-  // }
+  constructor(){
+    super()
+    this.state = ({
+      isEditingStatus: false,
+      editValue: '',
+      index: 0
+    })
+  }
 
   componentDidMount (){
     this.props.fetchTodos()
   }
 
+  changeEditingStatus(index) {
+    this.setState({
+      isEditingStatus: true,
+      index: index
+    })
+  }
+
+  handleChange(event) {
+    this.setState({
+      editValue: event.target.value
+    })
+  }
+
   render(){
     return (
-      <div>
-        <ul className="collection with-header">
-          {
-            this.props.todos.map((item, index) => {
-              return (
-                <li className="collection-item" key={index}>
-                  {/*<div>
-                    <form className="completed-checkbox">
-                      <p style={{ display: 'inline-block' }}>
-                        {item.is_completed}
-                        <input type="checkbox" checked={item.is_completed === "true" ? "checked" : ""}/>
-                      </p>
-                    </form>
-                  </div>*/}
-                  {item.title}, {JSON.stringify(item.is_completed)}
-                </li>
-              )
-            })
-          }
-        </ul>
+      <div className="container">
+        <div style={{ float: 'right' }}>
+          <form>
+            <i className="material-icons">search</i>
+            <input className="input-field col s10" type="text" placeholder="Search your task . . ." onChange={ (event) => this.props.searchTodo(event.target.value)}/>
+          </form>
+        </div>
+        <table className="striped responsive-table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Task</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.todos.map((todo, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{todo.id}</td>
+                    <td>
+                      {
+                        this.state.isEditingStatus && this.state.index === index ?
+                        <input type="text" onChange={(event) => this.handleChange(event)} defaultValue={todo.title} autoFocus/> : todo.title
+                      }
+                    </td>
+                    <td>
+                      <button className="waves-effect orange btn" type="button" onClick={() => { this.changeEditingStatus(index) }}>Edit</button>
+                      <button className="waves-effect orange btn" type="button" onClick={() => { this.props.deleteTodo(todo.id) }}>Delete</button>
+                    </td>
+                  </tr>
+                )
+              })}
+          </tbody>
+        </table>
       </div>
     )
   }
@@ -50,7 +80,10 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    fetchTodos: () => dispatch(fetchTodos())
+    fetchTodos: () => dispatch(fetchTodos()),
+    deleteTodo: (id) => dispatch(deleteTodo(id)),
+    updateTodo: (id, todo) => dispatch(updateTodo(id, todo)),
+    searchTodo: (todo) => dispatch(searchTodo(todo))
   }
 }
 
